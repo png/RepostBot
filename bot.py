9424448b085dc8b7a6972e86f74f65f8eebe7120
Josh Callsen <jcallsen@vt.edu>
2021-05-14 13:26:33 -0400

    Changed to mongodb and added basic link support

--- bot.py
+++ bot.py
@@ -1,23 +1,73 @@
 import os
-
-import discord
+import argparse
+import re
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+import discord
+import hashlib
 
+from db import Database
+
+
+def get_args():
+    '''
+    Gets args for this program
+
+    param:
+    None
+
+    return:
+    parsed arguments object
+    '''
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('username', default='data/',
+                        help='Username for mongodb', type=str)
+    parser.add_argument('password',
+                        help='Password for mongodb', type=str)
+    parser.add_argument('collection', help='Colleciton name', type=str)
+
+    return(parser.parse_args())
+
+
+args = get_args()
+
+load_dotenv()
+db = Database(args.username, args.password, args.collection)
+
+TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
+
+link_format = re.compile('http[s]?:\/\/.*')
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    # TODO images
 
-    # TODO check message for repost
-    # TDOO log message if unique
+    content = message.content
+    # only save links
 
-client.run(TOKEN)
+    if(link_format.match(content)):
+        content = content.encode('utf-8')
+        hash = hashlib.sha256(content).hexdigest()
+        result = db.cursor.find_one({'content': hash})
+        if(result):
+            await message.reply(f"Repost: {result['link']}")
+            await message.add_reaction('🚨')
+        else:
+            link = f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}"
+            print(f"Saving link: {link}")
+            db.cursor.insert_one({'link': link, 'content' : hash})
+
+
+if __name__ == '__main__':
+    client.run(TOKEN)
--- db.py
+++ db.py
@@ -1,29 +1,12 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-load_dotenv()
-
-DB_ENGINE = os.environ.get("DB_ENGINE")
-
-engine = create_engine(DB_ENGINE if DB_ENGINE is not None 
-        else 'sqlite+pysqlite:///:memory:', echo=True, future=True)
-
-Base = declarative_base()
-
-class School(Base):
-
-    __tablename__ = "woot"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)  
+from pymongo import MongoClient
+import urllib.parse
 
 
-    def __init__(self, name):
+class Database:
+    def __init__(self, username, password, collection):
+        username = urllib.parse.quote_plus(username)
+        password = urllib.parse.quote_plus(password)
 
-        self.name = name    
-
-
-Base.metadata.create_all(engine)
+        client = MongoClient('mongodb://%s:%s@127.0.0.1/?authSource=%s' % (username, password, collection))
+        db = getattr(client, collection)
+        self.cursor = db.Messages
